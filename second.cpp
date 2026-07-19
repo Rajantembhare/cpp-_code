99edd39ec3b990b9643df415121dce3107a6ea23
rajantembhare <tembharerajan459@gmail.com>
2026-07-19 07:55:52 +0530

    changes

--- second.cpp
+++ second.cpp
@@ -5,6 +5,7 @@ int main() {
     string name;       // use string for names
     float cgpa;
     int roll_num;
+    int idnum;
 
     cout << "Enter the name of student: ";
     cin >> name;
@@ -14,11 +15,14 @@ int main() {
 
     cout << "Enter the student roll number: ";
     cin >> roll_num;
+    cout<<"enter your id num:";
+    cin>>idnum;
 
     cout << "\n--- Student Details ---\n";
     cout << "Name: " << name << endl;
     cout << "CGPA: " << cgpa << endl;
     cout << "Roll Number: " << roll_num << endl;
+    cout<<"ID Num:"<< idnum <<endl;
 
     return 0;
 }
